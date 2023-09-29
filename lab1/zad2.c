d2d4c6c14b052949d5740da840dc819c42d73c90
A1N3X10N <145099773+A1N3X10N@users.noreply.github.com>
2023-09-29 13:26:41 +0500

    Add files via upload

---
+++ lab1/zad2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void)
+{
+int a,b,c;
+a = 43;
+b = 52;
+printf ("c = %d \n", a*b);
+return 0;
+}
---
+++ lab1/zad3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+int a,b,c;
+a=3;
+b=2;
+printf ("c= %d \n", a+b);
+printf ("a= %d \n", a);
+printf ("b= %d \n", b);
+}
